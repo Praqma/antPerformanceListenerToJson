8c584f8e448e902a4f9908d27078962165f3597a
Andrey Devyatkin <ady@praqma.net>
2017-01-25 16:40:05 +0100

    Disable expect header when loading data to elk

--- load_to_elk.sh
+++ load_to_elk.sh
@@ -3,6 +3,7 @@
 IP=$1
 FILE=$2
 INDEX=${3:-antprofiler}
+MAPPINGS="mappings.json"
 MESSAGE="Please run the command as follow: ${0} <elk ip> <file with data to upload> <index name, optional, default:AntProfiler>"
 
 if [ -z "${IP}" ] || [ -z "${FILE}" ]
@@ -11,5 +12,8 @@ then
   exit 1
 fi
 
-curl -XPUT http://${IP}:9200/${INDEX} --data-binary @mappings.json
-curl -XPOST "${IP}:9200/${INDEX}/_bulk?pretty" --data-binary @${FILE}
+echo "Create mappings from ${MAPPINGS}"
+curl -XPUT http://${IP}:9200/${INDEX} --data-binary "@${MAPPINGS}"
+
+echo "Load data from ${FILE}"
+curl -H "Expect:" -XPOST "${IP}:9200/${INDEX}/_bulk?pretty" --data-binary "@${FILE}"
